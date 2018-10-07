41b55a414de6b0b060a4818b931bfd0f22e7ea08
hoang <nhathoang989@gmail.com>
2018-10-07 23:32:21 +0700

    add files

---
+++ src/Mix.Heart/Domain/Core/ViewModels/IViewModel.cs
@@ -0,0 +1,10 @@
+﻿// Licensed to the Mixcore Foundation under one or more agreements.
+// The Mixcore Foundation licenses this file to you under the GNU General Public License v3.0.
+// See the LICENSE file in the project root for more information.
+
+namespace Mix.Domain.Data.ViewModels
+{
+    public interface IViewModel
+    {
+    }
+}
