208583ee1af464d7716d135750a67caf3fd61f70
Hoang Nguyen Nhat <hoang.nguyen@orientsoftware.net>
2021-06-06 09:24:35 +0700

    Update Repository

---
+++ src/Mix.Heart/Infrastructure/Repository/CommandRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mix.Heart.Repository
+{
+    public class CommandRepository<TDbContext, TEntity> : QueryRepository<TDbContext, TEntity>
+        where TDbContext : DbContext
+        where TEntity : class
+    {
+        public CommandRepository(TDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
---
+++ src/Mix.Heart/Model/PagingModel.cs
@@ -0,0 +1,12 @@
+﻿using Mix.Heart.Enums;
+
+namespace Mix.Heart.Model
+{
+    public class PagingModel : IPagingModel
+    {
+        public string SortBy { get; set; }
+        public SortDirection SortDirection { get; set; }
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+    }
+}
