41da5f6909463ac29086b5bf008b0b8b7a6e2ac7
Hoang Nguyen Nhat <hoang.nguyen@orientsoftware.net>
2022-04-05 16:12:40 +0700

    Add GetService Ext

--- src/Mix.Heart/Extensions/ServiceExtension.cs
+++ src/Mix.Heart/Extensions/ServiceExtension.cs
@@ -1,13 +1,18 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Mix.Heart.Entities;
+﻿using Mix.Heart.Entities;
 using Mix.Heart.Repository;
 using System;
 using System.Collections.Generic;
 
-namespace Mix.Heart.Extensions
+namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ServiceExtension
     {
+        public static T GetService<T>(this IServiceCollection services)
+        {
+            var sp = services.BuildServiceProvider();
+            return sp.GetService<T>();
+        }
+
         public static IServiceCollection AddRepositories(this IServiceCollection services, IEnumerable<Type> entities, Type dbContextType)
         {
             var queryRepo = typeof(QueryRepository<,,>);
