8937b85fe78138ecae09bd4d012e1682026fe0a3
Hoang Nguyen Nhat <hoang.nguyen@orientsoftware.net>
2022-04-15 01:14:13 +0700

    clean code

---
+++ src/Mix.Heart/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Mix.Heart.Entities.Cache;
+using Mix.Heart.Repository;
+using Mix.Heart.Services;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMixCache(this IServiceCollection services)
+        {
+
+            services.AddSingleton<MixCacheDbContext>();
+            services.AddSingleton<EntityRepository<MixCacheDbContext, MixCache, string>>();
+            services.AddSingleton<MixCacheService>();
+
+            return services;
+        }
+    }
+}
