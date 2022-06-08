1bfb17f11c84cd24b7fa8c3119b29d25c61f3fd3
Hoang Nguyen Nhat <hoang.nguyen@orientsoftware.net>
2022-06-08 10:30:48 +0700

    add generic UOW

---
+++ src/Mix.Heart/UnitOfWork/GenericUnitOfWorkInfo.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mix.Heart.UnitOfWork
+{
+    public class GenericUnitOfWorkInfo<T>: UnitOfWorkInfo
+        where T : DbContext
+    {
+        public T DbContext => (T)ActiveDbContext;
+        public GenericUnitOfWorkInfo(T dbContext): base(dbContext)
+        {
+        }
+    }
+}
