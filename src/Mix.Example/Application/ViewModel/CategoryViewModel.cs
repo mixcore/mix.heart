b2780415d5f058a8c6ea76365434962f87a25414
Restyled.io <commits@restyled.io>
2022-11-16 14:52:31 +0000

    Restyled by clang-format

--- src/Mix.Example/Application/ViewModel/CategoryViewModel.cs
+++ src/Mix.Example/Application/ViewModel/CategoryViewModel.cs
@@ -5,32 +5,21 @@ using Mix.Example.Infrastructure;
 using Mix.Example.Infrastructure.MixEntities;
 using Mix.Heart.ViewModel;
 
-namespace Mix.Example.Application.ViewModel
-{
-public class CategoryViewModel : ViewModelBase<MixDbContext, CategoryEntity, Guid, CategoryViewModel>
-{
-    public CategoryViewModel(CategoryEntity entity) : base(entity)
-    {
-    }
+namespace Mix.Example.Application.ViewModel {
+  public class CategoryViewModel
+      : ViewModelBase<MixDbContext, CategoryEntity, Guid, CategoryViewModel> {
+    public CategoryViewModel(CategoryEntity entity) : base(entity) {}
 
-    public string Name {
-        get;
-        set;
-    }
+    public string Name { get; set; }
 
-    public string Description {
-        get;
-        set;
-    }
+    public string Description { get; set; }
 
-    public ProductViewModel Product {
-        get;
-        set;
-    }
+    public ProductViewModel Product { get; set; }
 
-    protected override Task SaveEntityRelationshipAsync(CategoryEntity parentEntity, CancellationToken cancellationToken = default)
-    {
-        return Task.CompletedTask;
+    protected override Task
+    SaveEntityRelationshipAsync(CategoryEntity parentEntity,
+                                CancellationToken cancellationToken = default) {
+      return Task.CompletedTask;
     }
-}
+  }
 }
--- src/Mix.Example/Application/ViewModel/ProductViewModel.cs
+++ src/Mix.Example/Application/ViewModel/ProductViewModel.cs
@@ -8,55 +8,39 @@ using Mix.Example.Infrastructure;
 using Mix.Example.Infrastructure.MixEntities;
 using Mix.Heart.ViewModel;
 
-namespace Mix.Example.Application.ViewModel
-{
-public class ProductViewModel : ViewModelBase<MixDbContext, ProductEntity, Guid, ProductViewModel>
-{
-    public ProductViewModel(SaveProductDto dto) : base(dto)
-    {
-        MappDto(dto);
-    }
+namespace Mix.Example.Application.ViewModel {
+  public class ProductViewModel
+      : ViewModelBase<MixDbContext, ProductEntity, Guid, ProductViewModel> {
+    public ProductViewModel(SaveProductDto dto) : base(dto) { MappDto(dto); }
 
-    public string Name {
-        get;
-        set;
-    }
+    public string Name { get; set; }
 
-    public string Description {
-        get;
-        set;
-    }
+    public string Description { get; set; }
 
-    public string Producer {
-        get;
-        set;
-    }
+    public string Producer { get; set; }
 
     /// <summary>
     /// TODO: consider with JsonIgnore attribute
     /// </summary>
     [JsonIgnore]
     public Guid CategoryId {
-        get;
-        set;
+      get; set;
     }
 
     public List<ProductDetailViewModel> ProductDetails {
-        get;
-        set;
+      get; set;
     } = new List<ProductDetailViewModel>();
 
-    protected void MappDto(SaveProductDto dto)
-    {
-        foreach (var item in dto.ProductDetailDtos)
-        {
-            ProductDetails.Add(new ProductDetailViewModel(item));
-        }
+    protected void MappDto(SaveProductDto dto) {
+      foreach (var item in dto.ProductDetailDtos) {
+        ProductDetails.Add(new ProductDetailViewModel(item));
+      }
     }
 
-    protected override Task SaveEntityRelationshipAsync(ProductEntity parentEntity, CancellationToken cancellationToken = default)
-    {
-        return Task.CompletedTask;
+    protected override Task
+    SaveEntityRelationshipAsync(ProductEntity parentEntity,
+                                CancellationToken cancellationToken = default) {
+      return Task.CompletedTask;
     }
-}
+  }
 }
