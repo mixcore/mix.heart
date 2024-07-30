b168c483a5eafeccfb0d1d994e108d0ea6076aad
Restyled.io <commits@restyled.io>
2024-07-30 09:22:50 +0000

    Restyled by clang-format

--- src/mix.heart/Factories/CacheEngineFactory.cs
+++ src/mix.heart/Factories/CacheEngineFactory.cs
@@ -8,56 +8,51 @@ using Mix.Heart.Services;
 using Mix.Heart.UnitOfWork;
 using System;
 
-namespace Mix.Heart.Factories
-{
-public class CacheEngineFactory
-{
-    public static IDitributedCacheClient? CreateCacheClient(
-        MixHeartConfigurationModel mixHeartConfiguration,
-        UnitOfWorkInfo<MixCacheDbContext> uow = null,
-        IConfiguration configuration = null,
-        IDistributedCache cache = null)
-    {
-        IDitributedCacheClient cacheClient = null;
-        switch (mixHeartConfiguration.CacheMode)
-        {
-        case MixCacheMode.JSON:
-            cacheClient = new MixStaticFileCacheClient(mixHeartConfiguration.CacheFolder);
-            break;
-        case MixCacheMode.DATABASE:
-            cacheClient = new MixDatabaseCacheClient(uow);
-            break;
-        case MixCacheMode.REDIS:
-            try
-            {
-                var _configs = new RedisCacheConfigurationModel();
-                configuration.GetSection("Redis").Bind(_configs);
+namespace Mix.Heart.Factories {
+public class CacheEngineFactory {
+  public static IDitributedCacheClient? CreateCacheClient(
+      MixHeartConfigurationModel mixHeartConfiguration,
+      UnitOfWorkInfo<MixCacheDbContext> uow = null,
+      IConfiguration configuration = null, IDistributedCache cache = null) {
+    IDitributedCacheClient cacheClient = null;
+    switch (mixHeartConfiguration.CacheMode) {
+    case MixCacheMode.JSON:
+      cacheClient =
+          new MixStaticFileCacheClient(mixHeartConfiguration.CacheFolder);
+      break;
+    case MixCacheMode.DATABASE:
+      cacheClient = new MixDatabaseCacheClient(uow);
+      break;
+    case MixCacheMode.REDIS:
+      try {
+        var _configs = new RedisCacheConfigurationModel();
+        configuration.GetSection("Redis").Bind(_configs);
 
-                var options = new DistributedCacheEntryOptions()
-                {
-                    SlidingExpiration = TimeSpan.FromMinutes(_configs.SlidingExpirationInMinute),
-                };
-                if (_configs.AbsoluteExpirationInMinute.HasValue)
-                {
-                    options.SetAbsoluteExpiration(TimeSpan.FromMinutes(_configs.AbsoluteExpirationInMinute.Value));
-                }
-                if (_configs.AbsoluteExpirationRelativeToNowInMinute.HasValue)
-                {
-                    options.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(_configs.AbsoluteExpirationRelativeToNowInMinute.Value);
-                }
-
-                cacheClient = new RedisCacheClient(_configs.ConnectionString, cache, options);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Cannot create redis client: {ex.Message}, using JSON cache instead");
-                return default;
-            }
-            break;
-        default:
-            break;
+        var options = new DistributedCacheEntryOptions() {
+          SlidingExpiration =
+              TimeSpan.FromMinutes(_configs.SlidingExpirationInMinute),
+        };
+        if (_configs.AbsoluteExpirationInMinute.HasValue) {
+          options.SetAbsoluteExpiration(
+              TimeSpan.FromMinutes(_configs.AbsoluteExpirationInMinute.Value));
         }
-        return cacheClient;
+        if (_configs.AbsoluteExpirationRelativeToNowInMinute.HasValue) {
+          options.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(
+              _configs.AbsoluteExpirationRelativeToNowInMinute.Value);
+        }
+
+        cacheClient =
+            new RedisCacheClient(_configs.ConnectionString, cache, options);
+      } catch (Exception ex) {
+        Console.WriteLine(
+            $"Cannot create redis client: {ex.Message}, using JSON cache instead");
+        return default;
+      }
+      break;
+    default:
+      break;
     }
+    return cacheClient;
+  }
 }
 }
--- src/mix.heart/Services/RedisCacheClient.cs
+++ src/mix.heart/Services/RedisCacheClient.cs
@@ -7,64 +7,61 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Mix.Heart.Services
-{
-public class RedisCacheClient : IDitributedCacheClient
-{
-    private readonly IDatabase _database;
-    private readonly IDistributedCache _cache;
-    private readonly DistributedCacheEntryOptions _options;
-    private readonly ConnectionMultiplexer _connectionMultiplexer;
+namespace Mix.Heart.Services {
+public class RedisCacheClient : IDitributedCacheClient {
+  private readonly IDatabase _database;
+  private readonly IDistributedCache _cache;
+  private readonly DistributedCacheEntryOptions _options;
+  private readonly ConnectionMultiplexer _connectionMultiplexer;
 
-    public RedisCacheClient(string connectionString, IDistributedCache cache, DistributedCacheEntryOptions options)
-    {
-        _cache = cache;
-        _options = options;
-        _connectionMultiplexer = ConnectionMultiplexer.Connect(connectionString, m => m.AllowAdmin = true);
-        _database = _connectionMultiplexer.GetDatabase();
-    }
+  public RedisCacheClient(string connectionString, IDistributedCache cache,
+                          DistributedCacheEntryOptions options) {
+    _cache = cache;
+    _options = options;
+    _connectionMultiplexer = ConnectionMultiplexer.Connect(
+        connectionString, m => m.AllowAdmin = true);
+    _database = _connectionMultiplexer.GetDatabase();
+  }
 
-    public async Task<T> GetFromCache<T>(string key, CancellationToken cancellationToken = default) where T : class
-    {
-        var cachedResponse = await _cache.GetStringAsync(key, cancellationToken);
-        return cachedResponse == null ? null : JsonConvert.DeserializeObject<T>(cachedResponse);
-    }
+  public async Task<T>
+  GetFromCache<T>(string key, CancellationToken cancellationToken = default)
+      where T : class {
+    var cachedResponse = await _cache.GetStringAsync(key, cancellationToken);
+    return cachedResponse == null
+               ? null
+               : JsonConvert.DeserializeObject<T>(cachedResponse);
+  }
 
-    public async Task SetCache<T>(string key, T value, CancellationToken cancellationToken = default) where T : class
-    {
-        var response = JsonConvert.SerializeObject(value);
-        await _cache.SetStringAsync(key, response, _options, cancellationToken);
-    }
+  public async Task SetCache<T>(string key, T value,
+                                CancellationToken cancellationToken = default)
+      where T : class {
+    var response = JsonConvert.SerializeObject(value);
+    await _cache.SetStringAsync(key, response, _options, cancellationToken);
+  }
 
-    public async Task ClearCache(string key, CancellationToken cancellationToken = default)
-    {
-        var endpoints = _connectionMultiplexer.GetEndPoints();
-        foreach (var endpoint in endpoints)
-        {
-            var server = _connectionMultiplexer.GetServer(endpoint);
-            var keys = server.Keys(pattern: key + "*").ToList();
-            foreach (var k in keys)
-            {
-                await _database.KeyDeleteAsync(k);
-            }
-        }
+  public async Task ClearCache(string key,
+                               CancellationToken cancellationToken = default) {
+    var endpoints = _connectionMultiplexer.GetEndPoints();
+    foreach (var endpoint in endpoints) {
+      var server = _connectionMultiplexer.GetServer(endpoint);
+      var keys = server.Keys(pattern: key + "*").ToList();
+      foreach (var k in keys) {
+        await _database.KeyDeleteAsync(k);
+      }
     }
+  }
 
-    public async Task ClearAllCache(CancellationToken cancellationToken = default)
-    {
-        var endpoints = _connectionMultiplexer.GetEndPoints();
-        try
-        {
-            foreach (var endpoint in endpoints)
-            {
-                var server = _connectionMultiplexer.GetServer(endpoint);
-                await server.FlushDatabaseAsync();
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.Error.WriteLine(ex.Message);
-        }
+  public async
+      Task ClearAllCache(CancellationToken cancellationToken = default) {
+    var endpoints = _connectionMultiplexer.GetEndPoints();
+    try {
+      foreach (var endpoint in endpoints) {
+        var server = _connectionMultiplexer.GetServer(endpoint);
+        await server.FlushDatabaseAsync();
+      }
+    } catch (Exception ex) {
+      Console.Error.WriteLine(ex.Message);
     }
+  }
 }
 }
