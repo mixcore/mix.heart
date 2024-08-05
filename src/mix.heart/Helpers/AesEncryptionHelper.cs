bb113141bd3dd645d3628269208deb811ac3073c
nhathoang989 <nhathoang989@gmail.com>
2024-08-05 16:35:22 +0700

    fix encrypt

--- src/mix.heart/Helpers/AesEncryptionHelper.cs
+++ src/mix.heart/Helpers/AesEncryptionHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -6,7 +7,7 @@ namespace Mix.Heart.Helpers
 {
     public class AesEncryptionHelper
     {
-        public static string EncryptString(string text, string iCompleteEncodedKey)
+        public static string EncryptString(string text, string iCompleteEncodedKey, Encoding encoding = default)
         {
             if (text is null)
             {
@@ -18,56 +19,71 @@ namespace Mix.Heart.Helpers
 
             var iv = Convert.FromBase64String(keyStrings[0]);
             var key = Convert.FromBase64String(keyStrings[1]);
-            return EncryptString(text, key, iv);
+            return EncryptString(text, key, iv, encoding ?? Encoding.UTF8);
         }
 
-        public static string DecryptString(string cipherText, string iCompleteEncodedKey)
+        public static string DecryptString(string cipherText, string iCompleteEncodedKey, Encoding encoding = default)
         {
             string[] keyStrings =
                 Encoding.UTF8.GetString(Convert.FromBase64String(iCompleteEncodedKey))
                     .Split(',');
             var iv = Convert.FromBase64String(keyStrings[0]);
             var key = Convert.FromBase64String(keyStrings[1]);
-            return DecryptString(cipherText, key, iv);
+            return DecryptString(cipherText, key, iv, encoding ?? Encoding.UTF8);
         }
 
-        public static string EncryptString(string text, string keyString, string ivString)
+        public static string EncryptString(string text, string keyString, string ivString,Encoding encoding = default)
         {
             var iv = Encoding.UTF8.GetBytes(ivString);
             var key = Encoding.UTF8.GetBytes(keyString);
-            return EncryptString(text, key, iv);
+            return EncryptString(text, key, iv, encoding ?? Encoding.UTF8);
         }
 
-        public static string DecryptString(string cipherText, string keyString, string ivString)
+        public static string DecryptString(string cipherText, string keyString, string ivString, Encoding encoding = default)
         {
             var iv = Encoding.UTF8.GetBytes(ivString);
             var key = Encoding.UTF8.GetBytes(keyString);
-            return DecryptString(cipherText, key, iv);
+            return DecryptString(cipherText, key, iv, encoding ?? Encoding.UTF8);
         }
 
-        private static string EncryptString(string plainText, byte[] key, byte[] iv)
+        private static string EncryptString(string plainText, byte[] key, byte[] iv, Encoding encoding)
         {
             using var aesAlg = Aes.Create();
             aesAlg.BlockSize = 128;
             aesAlg.Mode = CipherMode.CBC;
             aesAlg.Padding = PaddingMode.PKCS7;
             using var encryptor = aesAlg.CreateEncryptor(key, iv);
-            byte[] bytes = Encoding.UTF8.GetBytes(plainText);
-            byte[] cipherText =
-                encryptor.TransformFinalBlock(bytes, 0, plainText.Length);
+            byte[] bytes = encoding.GetBytes(plainText);
+            byte[] cipherText;
+            using (var msEncrypt = new MemoryStream())
+            {
+                using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (var swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+                    cipherText = msEncrypt.ToArray();
+                }
+            }
             return Convert.ToBase64String(cipherText);
+
+
         }
 
-        private static string DecryptString(string cipherText, byte[] key, byte[] iv)
+        private static string DecryptString(string cipherText, byte[] key, byte[] iv, Encoding encoding)
         {
             using var aesAlg = Aes.Create();
             aesAlg.Mode = CipherMode.CBC;
             aesAlg.Padding = PaddingMode.PKCS7;
             using var decryptor = aesAlg.CreateDecryptor(key, iv);
-            byte[] encryptedBytes = Convert.FromBase64CharArray(
-                cipherText.ToCharArray(), 0, cipherText.Length);
-            return Encoding.UTF8.GetString(decryptor.TransformFinalBlock(
-                encryptedBytes, 0, encryptedBytes.Length));
+
+            using var msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText));
+            using var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
+            using var srDecrypt = new StreamReader(csDecrypt);
+            var plaintext = srDecrypt.ReadToEnd();
+
+            return plaintext;
         }
 
         public static string GenerateCombinedKeys()
