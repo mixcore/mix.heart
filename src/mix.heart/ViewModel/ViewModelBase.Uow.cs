41297946ba021e6a06f85598e54a8cb2614f7bab
Restyled.io <commits@restyled.io>
2024-09-06 08:53:07 +0000

    Restyled by astyle

--- src/mix.heart/ViewModel/ViewModelBase.Uow.cs
+++ src/mix.heart/ViewModel/ViewModelBase.Uow.cs
@@ -8,69 +8,69 @@ using System.Threading.Tasks;
 
 namespace Mix.Heart.ViewModel
 {
-    public abstract partial class ViewModelBase<TDbContext, TEntity, TPrimaryKey, TView>
+public abstract partial class ViewModelBase<TDbContext, TEntity, TPrimaryKey, TView>
+{
+    private bool _isRoot;
+
+    protected virtual void BeginUow()
     {
-        private bool _isRoot;
-
-        protected virtual void BeginUow()
+        if (UowInfo == null)
         {
-            if (UowInfo == null)
-            {
-                InitRootUow();
-            }
-
-            UowInfo.Begin();
-
-            if (Repository != null)
-            {
-                Repository.SetUowInfo(UowInfo);
-            }
-            else
-            {
-                Repository = GetRepository(UowInfo, CacheService);
-            }
-            Repository.SetCacheService(CacheService);
-            Repository.CacheFolder = CacheFolder;
+            InitRootUow();
         }
 
-        protected virtual void InitRootUow()
+        UowInfo.Begin();
+
+        if (Repository != null)
         {
-            UowInfo ??= new UnitOfWorkInfo(InitDbContext());
-            _isRoot = true;
+            Repository.SetUowInfo(UowInfo);
         }
-
-        protected virtual async Task CloseUowAsync()
+        else
         {
-            if (_isRoot)
-            {
-                await UowInfo.CloseAsync();
-            }
+            Repository = GetRepository(UowInfo, CacheService);
         }
+        Repository.SetCacheService(CacheService);
+        Repository.CacheFolder = CacheFolder;
+    }
 
-        protected virtual async Task CompleteUowAsync(CancellationToken cancellationToken = default)
+    protected virtual void InitRootUow()
+    {
+        UowInfo ??= new UnitOfWorkInfo(InitDbContext());
+        _isRoot = true;
+    }
+
+    protected virtual async Task CloseUowAsync()
+    {
+        if (_isRoot)
         {
-            if (_isRoot)
-            {
-                await UowInfo.CompleteAsync(cancellationToken);
-                return;
-            };
-
-            _isRoot = false;
-        }
-
-        protected virtual TDbContext InitDbContext()
-        {
-            var dbContextType = typeof(TDbContext);
-            var contextCtorInfo = dbContextType.GetConstructor(new Type[] { });
-
-            if (contextCtorInfo == null)
-            {
-                throw new MixException(
-                        MixErrorStatus.ServerError,
-                        $"{dbContextType}: Contructor Parameterless Notfound");
-            }
-
-            return (TDbContext)contextCtorInfo.Invoke([]);
+            await UowInfo.CloseAsync();
         }
     }
+
+    protected virtual async Task CompleteUowAsync(CancellationToken cancellationToken = default)
+    {
+        if (_isRoot)
+        {
+            await UowInfo.CompleteAsync(cancellationToken);
+            return;
+        };
+
+        _isRoot = false;
+    }
+
+    protected virtual TDbContext InitDbContext()
+    {
+        var dbContextType = typeof(TDbContext);
+        var contextCtorInfo = dbContextType.GetConstructor(new Type[] { });
+
+        if (contextCtorInfo == null)
+        {
+            throw new MixException(
+                MixErrorStatus.ServerError,
+                $"{dbContextType}: Contructor Parameterless Notfound");
+        }
+
+        return (TDbContext)contextCtorInfo.Invoke([]);
+    }
+}
 }
--- src/mix.heart/ViewModel/ViewModelBase.cs
+++ src/mix.heart/ViewModel/ViewModelBase.cs
@@ -11,126 +11,150 @@ using System.Threading.Tasks;
 
 namespace Mix.Heart.ViewModel
 {
-    public abstract partial class ViewModelBase<TDbContext, TEntity, TPrimaryKey, TView> : ViewModelQueryBase<TDbContext, TEntity, TPrimaryKey, TView>
-        where TPrimaryKey : IComparable
-        where TEntity : class, IEntity<TPrimaryKey>
-        where TDbContext : DbContext
-        where TView : ViewModelBase<TDbContext, TEntity, TPrimaryKey, TView>
+public abstract partial class ViewModelBase<TDbContext, TEntity, TPrimaryKey, TView> : ViewModelQueryBase<TDbContext, TEntity, TPrimaryKey, TView>
+    where TPrimaryKey : IComparable
+    where TEntity : class, IEntity<TPrimaryKey>
+    where TDbContext : DbContext
+    where TView : ViewModelBase<TDbContext, TEntity, TPrimaryKey, TView>
+{
+    #region Properties
+
+    public TPrimaryKey Id {
+        get;
+        set;
+    }
+
+    public DateTime CreatedDateTime {
+        get;
+        set;
+    }
+
+    public DateTime? LastModified {
+        get;
+        set;
+    }
+
+    public string CreatedBy {
+        get;
+        set;
+    }
+
+    public string ModifiedBy {
+        get;
+        set;
+    }
+
+    public int Priority {
+        get;
+        set;
+    }
+
+    public MixContentStatus Status {
+        get;
+        set;
+    } = MixContentStatus.Published;
+
+    public bool IsDeleted {
+        get;
+        set;
+    }
+
+    #endregion
+
+    #region Constructors
+
+    public ViewModelBase() : base()
     {
-        #region Properties
+    }
 
-        public TPrimaryKey Id { get; set; }
+    public ViewModelBase(TDbContext context) : base(context)
+    {
+        _isRoot = true;
+    }
 
-        public DateTime CreatedDateTime { get; set; }
+    public ViewModelBase(TEntity entity, UnitOfWorkInfo uowInfo) : base(entity, uowInfo)
+    {
+    }
 
-        public DateTime? LastModified { get; set; }
+    public ViewModelBase(UnitOfWorkInfo unitOfWorkInfo) : base(unitOfWorkInfo)
+    {
+    }
 
-        public string CreatedBy { get; set; }
+    #endregion
 
-        public string ModifiedBy { get; set; }
+    #region Abstracts
+    public virtual void InitDefaultValues(string language = null, int? cultureId = null)
+    {
+        CreatedDateTime = DateTime.UtcNow;
+        Status = MixContentStatus.Published;
+        IsDeleted = false;
+    }
 
-        public int Priority { get; set; }
+    #endregion
 
-        public MixContentStatus Status { get; set; } = MixContentStatus.Published;
-
-        public bool IsDeleted { get; set; }
-
-        #endregion
-
-        #region Constructors
-
-        public ViewModelBase() : base()
-        {
-        }
-
-        public ViewModelBase(TDbContext context) : base(context)
-        {
-            _isRoot = true;
-        }
-
-        public ViewModelBase(TEntity entity, UnitOfWorkInfo uowInfo) : base(entity, uowInfo)
-        {
-        }
-
-        public ViewModelBase(UnitOfWorkInfo unitOfWorkInfo) : base(unitOfWorkInfo)
-        {
-        }
-
-        #endregion
-
-        #region Abstracts
-        public virtual void InitDefaultValues(string language = null, int? cultureId = null)
-        {
-            CreatedDateTime = DateTime.UtcNow;
-            Status = MixContentStatus.Published;
-            IsDeleted = false;
-        }
-
-        #endregion
-
-        public virtual async Task Validate(CancellationToken cancellationToken)
-        {
-            cancellationToken.ThrowIfCancellationRequested();
-            if (!IsValid)
-            {
-                await HandleExceptionAsync(new MixException(MixErrorStatus.Badrequest, Errors.Select(e => e.ErrorMessage).ToArray()));
-            }
-        }
-
-        public void SetDbContext(TDbContext context)
-        {
-            UowInfo = new UnitOfWorkInfo(context);
-        }
-
-        public virtual TEntity InitModel()
-        {
-            Type classType = typeof(TEntity);
-            return (TEntity)Activator.CreateInstance(classType);
-        }
-
-        public virtual Task<TEntity> ParseEntity(CancellationToken cancellationToken = default)
-        {
-            cancellationToken.ThrowIfCancellationRequested();
-
-            if (IsDefaultId(Id))
-            {
-                InitDefaultValues();
-            }
-
-            var entity = Activator.CreateInstance<TEntity>();
-            ReflectionHelper.Map(this as TView, entity);
-            return Task.FromResult(entity);
-        }
-
-        public bool IsDefaultId(TPrimaryKey id)
-        {
-            return (id.GetType() == typeof(Guid) && Guid.Parse(id.ToString()) == Guid.Empty)
-                || (id.GetType() == typeof(int) && int.Parse(id.ToString()) == default);
-        }
-
-        public virtual Task DuplicateAsync(CancellationToken cancellationToken = default)
-        {
-            cancellationToken.ThrowIfCancellationRequested();
-            return Task.CompletedTask;
-        }
-
-        public virtual void Duplicate()
-        {
-        }
-
-        protected async Task HandleErrorsAsync()
+    public virtual async Task Validate(CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        if (!IsValid)
         {
             await HandleExceptionAsync(new MixException(MixErrorStatus.Badrequest, Errors.Select(e => e.ErrorMessage).ToArray()));
         }
-
-        protected virtual async Task HandleExceptionAsync(Exception ex)
-        {
-            await Repository.HandleExceptionAsync(ex);
-        }
-
-        protected virtual void HandleException(Exception ex)
-        {
-            Repository.HandleException(ex);
-        }
     }
+
+    public void SetDbContext(TDbContext context)
+    {
+        UowInfo = new UnitOfWorkInfo(context);
+    }
+
+    public virtual TEntity InitModel()
+    {
+        Type classType = typeof(TEntity);
+        return (TEntity)Activator.CreateInstance(classType);
+    }
+
+    public virtual Task<TEntity> ParseEntity(CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        if (IsDefaultId(Id))
+        {
+            InitDefaultValues();
+        }
+
+        var entity = Activator.CreateInstance<TEntity>();
+        ReflectionHelper.Map(this as TView, entity);
+        return Task.FromResult(entity);
+    }
+
+    public bool IsDefaultId(TPrimaryKey id)
+    {
+        return (id.GetType() == typeof(Guid) && Guid.Parse(id.ToString()) == Guid.Empty)
+               || (id.GetType() == typeof(int) && int.Parse(id.ToString()) == default);
+    }
+
+    public virtual Task DuplicateAsync(CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        return Task.CompletedTask;
+    }
+
+    public virtual void Duplicate()
+    {
+    }
+
+    protected async Task HandleErrorsAsync()
+    {
+        await HandleExceptionAsync(new MixException(MixErrorStatus.Badrequest, Errors.Select(e => e.ErrorMessage).ToArray()));
+    }
+
+    protected virtual async Task HandleExceptionAsync(Exception ex)
+    {
+        await Repository.HandleExceptionAsync(ex);
+    }
+
+    protected virtual void HandleException(Exception ex)
+    {
+        Repository.HandleException(ex);
+    }
+}
 }
