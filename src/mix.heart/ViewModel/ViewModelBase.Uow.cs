459d9e254049f55b299bed446f094a8fad73027e
Restyled.io <commits@restyled.io>
2024-09-06 08:53:14 +0000

    Restyled by clang-format

--- src/mix.heart/ViewModel/ViewModelBase.Uow.cs
+++ src/mix.heart/ViewModel/ViewModelBase.Uow.cs
@@ -6,71 +6,59 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Mix.Heart.ViewModel
-{
-public abstract partial class ViewModelBase<TDbContext, TEntity, TPrimaryKey, TView>
-{
-    private bool _isRoot;
+namespace Mix.Heart.ViewModel {
+public abstract
+    partial class ViewModelBase<TDbContext, TEntity, TPrimaryKey, TView> {
+  private bool _isRoot;
 
-    protected virtual void BeginUow()
-    {
-        if (UowInfo == null)
-        {
-            InitRootUow();
-        }
-
-        UowInfo.Begin();
-
-        if (Repository != null)
-        {
-            Repository.SetUowInfo(UowInfo);
-        }
-        else
-        {
-            Repository = GetRepository(UowInfo, CacheService);
-        }
-        Repository.SetCacheService(CacheService);
-        Repository.CacheFolder = CacheFolder;
+  protected virtual void BeginUow() {
+    if (UowInfo == null) {
+      InitRootUow();
     }
 
-    protected virtual void InitRootUow()
-    {
-        UowInfo ??= new UnitOfWorkInfo(InitDbContext());
-        _isRoot = true;
+    UowInfo.Begin();
+
+    if (Repository != null) {
+      Repository.SetUowInfo(UowInfo);
+    } else {
+      Repository = GetRepository(UowInfo, CacheService);
+    }
+    Repository.SetCacheService(CacheService);
+    Repository.CacheFolder = CacheFolder;
+  }
+
+  protected virtual void InitRootUow() {
+    UowInfo ??= new UnitOfWorkInfo(InitDbContext());
+    _isRoot = true;
+  }
+
+  protected virtual async Task CloseUowAsync() {
+    if (_isRoot) {
+      await UowInfo.CloseAsync();
+    }
+  }
+
+  protected virtual async
+      Task CompleteUowAsync(CancellationToken cancellationToken = default) {
+    if (_isRoot) {
+      await UowInfo.CompleteAsync(cancellationToken);
+      return;
+    };
+
+    _isRoot = false;
+  }
+
+  protected virtual TDbContext InitDbContext() {
+    var dbContextType = typeof(TDbContext);
+    var contextCtorInfo = dbContextType.GetConstructor(new Type[] {});
+
+    if (contextCtorInfo == null) {
+      throw new MixException(
+          MixErrorStatus.ServerError,
+          $"{dbContextType}: Contructor Parameterless Notfound");
     }
 
-    protected virtual async Task CloseUowAsync()
-    {
-        if (_isRoot)
-        {
-            await UowInfo.CloseAsync();
-        }
-    }
-
-    protected virtual async Task CompleteUowAsync(CancellationToken cancellationToken = default)
-    {
-        if (_isRoot)
-        {
-            await UowInfo.CompleteAsync(cancellationToken);
-            return;
-        };
-
-        _isRoot = false;
-    }
-
-    protected virtual TDbContext InitDbContext()
-    {
-        var dbContextType = typeof(TDbContext);
-        var contextCtorInfo = dbContextType.GetConstructor(new Type[] { });
-
-        if (contextCtorInfo == null)
-        {
-            throw new MixException(
-                MixErrorStatus.ServerError,
-                $"{dbContextType}: Contructor Parameterless Notfound");
-        }
-
-        return (TDbContext)contextCtorInfo.Invoke([]);
-    }
+    return (TDbContext)contextCtorInfo.Invoke([]);
+  }
 }
 }
--- src/mix.heart/ViewModel/ViewModelQueryBase.cs
+++ src/mix.heart/ViewModel/ViewModelQueryBase.cs
@@ -12,140 +12,109 @@ using Mix.Heart.Model;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 
-namespace Mix.Heart.ViewModel
-{
-public abstract class ViewModelQueryBase<TDbContext, TEntity, TPrimaryKey, TView>
+namespace Mix.Heart.ViewModel {
+public abstract class ViewModelQueryBase<TDbContext, TEntity, TPrimaryKey,
+                                         TView>
     where TPrimaryKey : IComparable
     where TEntity : class, IEntity<TPrimaryKey>
     where TDbContext : DbContext
-    where TView : ViewModelQueryBase<TDbContext, TEntity, TPrimaryKey, TView>
-{
-    protected ValidationContext ValidateContext;
+    where TView : ViewModelQueryBase<TDbContext, TEntity, TPrimaryKey, TView> {
+  protected ValidationContext ValidateContext;
 
-    [JsonIgnore]
-    public static bool IsCache {
-        get;
-        set;
-    } = true;
+  [JsonIgnore]
+  public static bool IsCache { get; set; } = true;
 
-    [JsonIgnore]
-    public static string CacheFolder {
-        get;
-        set;
-    } = $"{typeof(TEntity).Assembly.GetName().Name}_{typeof(TEntity).Name}";
+  [JsonIgnore]
+  public static string CacheFolder { get; set; } =
+      $"{typeof(TEntity).Assembly.GetName().Name}_{typeof(TEntity).Name}";
 
-    [JsonIgnore]
-    protected bool IsValid {
-        get;
-        set;
+  [JsonIgnore]
+  protected bool IsValid { get; set; }
+
+  [JsonIgnore]
+  protected UnitOfWorkInfo UowInfo { get; set; }
+
+  [JsonIgnore]
+  protected MixCacheService CacheService { get; set; }
+
+  [JsonIgnore]
+  protected List<ValidationResult> Errors { get; set; } = [];
+
+  [JsonIgnore]
+  protected Repository<TDbContext, TEntity, TPrimaryKey, TView> Repository {
+      get; set; }
+
+  [JsonIgnore]
+  public List<ModifiedEntityModel> ModifiedEntities { get; set; } = [];
+
+  protected TDbContext Context { get => (TDbContext)UowInfo?.ActiveDbContext; }
+
+  public ViewModelQueryBase() {
+    ValidateContext =
+        new ValidationContext(this, serviceProvider: null, items: null);
+    Repository ??= GetRepository(UowInfo, CacheService);
+  }
+
+  public ViewModelQueryBase(TDbContext context) {
+    ValidateContext =
+        new ValidationContext(this, serviceProvider: null, items: null);
+    UowInfo = new UnitOfWorkInfo(context);
+    Repository ??= GetRepository(UowInfo, CacheService);
+  }
+
+  public ViewModelQueryBase(TEntity entity, UnitOfWorkInfo uowInfo) {
+    ValidateContext =
+        new ValidationContext(this, serviceProvider: null, items: null);
+    SetUowInfo(uowInfo, CacheService);
+    ParseView(entity);
+  }
+
+  public ViewModelQueryBase(UnitOfWorkInfo unitOfWorkInfo) {
+    ValidateContext =
+        new ValidationContext(this, serviceProvider: null, items: null);
+    SetUowInfo(unitOfWorkInfo, CacheService);
+  }
+
+  public virtual Task
+  ExpandView(CancellationToken cancellationToken = default) {
+    cancellationToken.ThrowIfCancellationRequested();
+    return Task.CompletedTask;
+  }
+
+  public virtual void
+  ParseView<TSource>(TSource sourceObject,
+                     CancellationToken cancellationToken = default)
+      where TSource : TEntity {
+    cancellationToken.ThrowIfCancellationRequested();
+    ReflectionHelper.Map(sourceObject, this as TView);
+  }
+
+  public void SetUowInfo(UnitOfWorkInfo unitOfWorkInfo,
+                         MixCacheService cacheService) {
+    if (unitOfWorkInfo != null) {
+      UowInfo = unitOfWorkInfo;
+      Repository ??= GetRepository(UowInfo, CacheService);
     }
 
-    [JsonIgnore]
-    protected UnitOfWorkInfo UowInfo {
-        get;
-        set;
-    }
+    SetCacheService(cacheService);
+  }
 
-    [JsonIgnore]
-    protected MixCacheService CacheService {
-        get;
-        set;
-    }
+  public void SetCacheService(MixCacheService cacheService) {
+    CacheService ??= cacheService;
+  }
 
-    [JsonIgnore]
-    protected List<ValidationResult> Errors {
-        get;
-        set;
-    } = [];
+  public static Repository<TDbContext, TEntity, TPrimaryKey, TView>
+  GetRepository(UnitOfWorkInfo uowInfo, MixCacheService cacheService,
+                bool isCache = true, string cacheFolder = null) {
+    return new Repository<TDbContext, TEntity, TPrimaryKey, TView>(
+        uowInfo) { IsCache = isCache, CacheFolder = cacheFolder ?? CacheFolder,
+                   CacheService = cacheService };
+  }
 
-    [JsonIgnore]
-    protected Repository<TDbContext, TEntity, TPrimaryKey, TView> Repository {
-        get;
-        set;
-    }
-
-    [JsonIgnore]
-    public List<ModifiedEntityModel> ModifiedEntities {
-        get;
-        set;
-    } = [];
-
-    protected TDbContext Context {
-        get => (TDbContext)UowInfo?.ActiveDbContext;
-    }
-
-    public ViewModelQueryBase()
-    {
-        ValidateContext = new ValidationContext(this, serviceProvider: null, items: null);
-        Repository ??= GetRepository(UowInfo, CacheService);
-    }
-
-    public ViewModelQueryBase(TDbContext context)
-    {
-        ValidateContext = new ValidationContext(this, serviceProvider: null, items: null);
-        UowInfo = new UnitOfWorkInfo(context);
-        Repository ??= GetRepository(UowInfo, CacheService);
-    }
-
-    public ViewModelQueryBase(TEntity entity, UnitOfWorkInfo uowInfo)
-    {
-        ValidateContext = new ValidationContext(this, serviceProvider: null, items: null);
-        SetUowInfo(uowInfo, CacheService);
-        ParseView(entity);
-    }
-
-    public ViewModelQueryBase(UnitOfWorkInfo unitOfWorkInfo)
-    {
-        ValidateContext = new ValidationContext(this, serviceProvider: null, items: null);
-        SetUowInfo(unitOfWorkInfo, CacheService);
-    }
-
-    public virtual Task ExpandView(CancellationToken cancellationToken = default)
-    {
-        cancellationToken.ThrowIfCancellationRequested();
-        return Task.CompletedTask;
-    }
-
-    public virtual void ParseView<TSource>(TSource sourceObject, CancellationToken cancellationToken = default)
-    where TSource : TEntity
-    {
-        cancellationToken.ThrowIfCancellationRequested();
-        ReflectionHelper.Map(sourceObject, this as TView);
-    }
-
-    public void SetUowInfo(UnitOfWorkInfo unitOfWorkInfo, MixCacheService cacheService)
-    {
-        if (unitOfWorkInfo != null)
-        {
-            UowInfo = unitOfWorkInfo;
-            Repository ??= GetRepository(UowInfo, CacheService);
-        }
-
-        SetCacheService(cacheService);
-    }
-
-    public void SetCacheService(MixCacheService cacheService)
-    {
-        CacheService ??= cacheService;
-    }
-
-    public static Repository<TDbContext, TEntity, TPrimaryKey, TView> GetRepository(UnitOfWorkInfo uowInfo, MixCacheService cacheService, bool isCache = true, string cacheFolder = null)
-    {
-        return new Repository<TDbContext, TEntity, TPrimaryKey, TView>(uowInfo)
-        {
-            IsCache = isCache,
-            CacheFolder = cacheFolder ?? CacheFolder,
-            CacheService = cacheService
-        };
-    }
-
-    public static Repository<TDbContext, TEntity, TPrimaryKey, TView> GetRootRepository(TDbContext context, MixCacheService cacheService)
-    {
-        return new Repository<TDbContext, TEntity, TPrimaryKey, TView>(context)
-        {
-            IsCache = cacheService != null,
-            CacheFolder = CacheFolder
-        };
-    }
+  public static Repository<TDbContext, TEntity, TPrimaryKey, TView>
+  GetRootRepository(TDbContext context, MixCacheService cacheService) {
+    return new Repository<TDbContext, TEntity, TPrimaryKey, TView>(
+        context) { IsCache = cacheService != null, CacheFolder = CacheFolder };
+  }
 }
 }
