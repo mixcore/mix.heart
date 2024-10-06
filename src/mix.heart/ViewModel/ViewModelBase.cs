d41282a4974b1f5746ddb61542bdafa8f9e439e8
Restyled.io <commits@restyled.io>
2024-10-06 16:34:58 +0000

    Restyled by clang-format

--- src/mix.heart/ViewModel/ViewModelBase.cs
+++ src/mix.heart/ViewModel/ViewModelBase.cs
@@ -4,82 +4,54 @@ using Mix.Heart.Enums;
 using Mix.Heart.UnitOfWork;
 using System;
 
-namespace Mix.Heart.ViewModel
-{
-public abstract partial class ViewModelBase<TDbContext, TEntity, TPrimaryKey, TView> : SimpleViewModelBase<TDbContext, TEntity, TPrimaryKey, TView>
+namespace Mix.Heart.ViewModel {
+public abstract
+    partial class ViewModelBase<TDbContext, TEntity, TPrimaryKey, TView>
+    : SimpleViewModelBase<TDbContext, TEntity, TPrimaryKey, TView>
     where TPrimaryKey : IComparable
     where TEntity : class, IEntity<TPrimaryKey>
     where TDbContext : DbContext
-    where TView : ViewModelBase<TDbContext, TEntity, TPrimaryKey, TView>
-{
-    #region Properties
+    where TView : ViewModelBase<TDbContext, TEntity, TPrimaryKey, TView> {
+#region Properties
 
-    public DateTime CreatedDateTime {
-        get;
-        set;
-    }
+  public DateTime CreatedDateTime { get; set; }
 
-    public DateTime? LastModified {
-        get;
-        set;
-    }
+  public DateTime? LastModified { get; set; }
 
-    public string CreatedBy {
-        get;
-        set;
-    }
+  public string CreatedBy { get; set; }
 
-    public string ModifiedBy {
-        get;
-        set;
-    }
+  public string ModifiedBy { get; set; }
 
-    public int Priority {
-        get;
-        set;
-    }
+  public int Priority { get; set; }
 
-    public MixContentStatus Status {
-        get;
-        set;
-    } = MixContentStatus.Published;
+  public MixContentStatus Status { get; set; } = MixContentStatus.Published;
 
-    public bool IsDeleted {
-        get;
-        set;
-    }
+  public bool IsDeleted { get; set; }
 
-    #endregion
+#endregion
 
-    #region Constructors
+#region Constructors
 
-    public ViewModelBase() : base()
-    {
-    }
+  public ViewModelBase() : base() {}
 
-    public ViewModelBase(TDbContext context) : base(context)
-    {
-    }
+  public ViewModelBase(TDbContext context) : base(context) {}
 
-    public ViewModelBase(TEntity entity, UnitOfWorkInfo uowInfo) : base(entity, uowInfo)
-    {
-    }
+  public ViewModelBase(TEntity entity, UnitOfWorkInfo uowInfo)
+      : base(entity, uowInfo) {}
 
-    public ViewModelBase(UnitOfWorkInfo unitOfWorkInfo) : base(unitOfWorkInfo)
-    {
-    }
+  public ViewModelBase(UnitOfWorkInfo unitOfWorkInfo) : base(unitOfWorkInfo) {}
 
-    #endregion
+#endregion
 
-    #region Overrides
+#region Overrides
 
-    public override void InitDefaultValues(string language = null, int? cultureId = null)
-    {
-        CreatedDateTime = DateTime.UtcNow;
-        Status = MixContentStatus.Published;
-        IsDeleted = false;
-    }
+  public override void InitDefaultValues(string language = null,
+                                         int? cultureId = null) {
+    CreatedDateTime = DateTime.UtcNow;
+    Status = MixContentStatus.Published;
+    IsDeleted = false;
+  }
 
-    #endregion
+#endregion
 }
 }
