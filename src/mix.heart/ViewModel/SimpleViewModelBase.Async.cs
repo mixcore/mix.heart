f437c1de2af42544986fef790c9afe81ab4ffd4e
Restyled.io <commits@restyled.io>
2024-10-06 16:34:57 +0000

    Restyled by astyle

--- src/mix.heart/ViewModel/SimpleViewModelBase.Async.cs
+++ src/mix.heart/ViewModel/SimpleViewModelBase.Async.cs
@@ -11,132 +11,132 @@ using System.Threading.Tasks;
 
 namespace Mix.Heart.ViewModel
 {
-    public abstract partial class SimpleViewModelBase<TDbContext, TEntity, TPrimaryKey, TView>
+public abstract partial class SimpleViewModelBase<TDbContext, TEntity, TPrimaryKey, TView>
+{
+    #region Async
+
+    public virtual async Task DeleteAsync(CancellationToken cancellationToken = default)
     {
-        #region Async
-
-        public virtual async Task DeleteAsync(CancellationToken cancellationToken = default)
-        {
-            try
-            {
-                cancellationToken.ThrowIfCancellationRequested();
-                BeginUow();
-                await DeleteHandlerAsync(cancellationToken);
-                await CompleteUowAsync(cancellationToken);
-            }
-            catch (Exception ex)
-            {
-                await HandleExceptionAsync(ex);
-            }
-            finally
-            {
-                await CloseUowAsync();
-            }
-        }
-
-        protected virtual async Task DeleteHandlerAsync(CancellationToken cancellationToken = default)
+        try
         {
             cancellationToken.ThrowIfCancellationRequested();
-            Repository.SetUowInfo(UowInfo);
-            await Repository.DeleteAsync(Id, cancellationToken);
-            ModifiedEntities.Add(new(typeof(TEntity), Id, ViewModelAction.Delete));
-            await DeleteEntityRelationshipAsync(cancellationToken);
+            BeginUow();
+            await DeleteHandlerAsync(cancellationToken);
+            await CompleteUowAsync(cancellationToken);
         }
-
-        public virtual async Task<TPrimaryKey> SaveAsync(CancellationToken cancellationToken = default)
+        catch (Exception ex)
         {
-            try
-            {
-                cancellationToken.ThrowIfCancellationRequested();
-                BeginUow();
-                IsValid = Validator.TryValidateObject(this, ValidateContext, Errors);
-                await Validate(cancellationToken);
-                if (!IsValid)
-                {
-                    await HandleExceptionAsync(new MixException(MixErrorStatus.Badrequest, Errors.Select(e => e.ErrorMessage).ToArray()));
-                }
-                var entity = await SaveHandlerAsync(cancellationToken);
-                await CompleteUowAsync(cancellationToken);
-                return entity.Id;
-            }
-            catch (Exception ex)
-            {
-                await HandleExceptionAsync(ex);
-                return default;
-            }
-            finally
-            {
-                await CloseUowAsync();
-            }
+            await HandleExceptionAsync(ex);
         }
-
-        public virtual async Task<TPrimaryKey> SaveFieldsAsync(IEnumerable<EntityPropertyModel> properties, CancellationToken cancellationToken = default)
+        finally
         {
-            try
-            {
-                cancellationToken.ThrowIfCancellationRequested();
-                BeginUow();
-                foreach (var property in properties)
-                {
-                    // check if field name is exist
-                    var lamda = ReflectionHelper.GetLambda<TEntity>(property.PropertyName);
-                    if (lamda != null)
-                    {
-                        ReflectionHelper.SetPropertyValue(this, property);
-                    }
-                    else
-                    {
-                        await HandleExceptionAsync(new MixException(MixErrorStatus.Badrequest, $"Invalid Property {property.PropertyName}"));
-                    }
-                }
-                IsValid = Validator.TryValidateObject(this, ValidateContext, Errors);
-                await Validate(cancellationToken);
-                var entity = await ParseEntity(cancellationToken);
-                await Repository.SaveAsync(entity, cancellationToken);
-                await CompleteUowAsync(cancellationToken);
-                return entity.Id;
-            }
-            catch (Exception ex)
-            {
-                await HandleExceptionAsync(ex);
-                return default;
-            }
-            finally
-            {
-                await CloseUowAsync();
-            }
+            await CloseUowAsync();
         }
-
-        #region virtual methods
-
-        // Override this method
-        protected virtual async Task<TEntity> SaveHandlerAsync(CancellationToken cancellationToken = default)
-        {
-            cancellationToken.ThrowIfCancellationRequested();
-            var entity = await ParseEntity(cancellationToken);
-
-            ModifiedEntities.Add(new(typeof(TEntity), Id, !Id.Equals(default) ? ViewModelAction.Create : ViewModelAction.Update));
-
-            await Repository.SaveAsync(entity, cancellationToken);
-            await SaveEntityRelationshipAsync(entity, cancellationToken);
-            Id = entity.Id;
-            return entity;
-        }
-
-        // Override this method
-        protected virtual Task SaveEntityRelationshipAsync(TEntity parentEntity, CancellationToken cancellationToken = default)
-        {
-            cancellationToken.ThrowIfCancellationRequested();
-            return Task.CompletedTask;
-        }
-
-        protected virtual Task DeleteEntityRelationshipAsync(CancellationToken cancellationToken = default)
-        {
-            return Task.CompletedTask;
-        }
-
-        #endregion
-
-        #endregion
     }
+
+    protected virtual async Task DeleteHandlerAsync(CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        Repository.SetUowInfo(UowInfo);
+        await Repository.DeleteAsync(Id, cancellationToken);
+        ModifiedEntities.Add(new(typeof(TEntity), Id, ViewModelAction.Delete));
+        await DeleteEntityRelationshipAsync(cancellationToken);
+    }
+
+    public virtual async Task<TPrimaryKey> SaveAsync(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            BeginUow();
+            IsValid = Validator.TryValidateObject(this, ValidateContext, Errors);
+            await Validate(cancellationToken);
+            if (!IsValid)
+            {
+                await HandleExceptionAsync(new MixException(MixErrorStatus.Badrequest, Errors.Select(e => e.ErrorMessage).ToArray()));
+            }
+            var entity = await SaveHandlerAsync(cancellationToken);
+            await CompleteUowAsync(cancellationToken);
+            return entity.Id;
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(ex);
+            return default;
+        }
+        finally
+        {
+            await CloseUowAsync();
+        }
+    }
+
+    public virtual async Task<TPrimaryKey> SaveFieldsAsync(IEnumerable<EntityPropertyModel> properties, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            BeginUow();
+            foreach (var property in properties)
+            {
+                // check if field name is exist
+                var lamda = ReflectionHelper.GetLambda<TEntity>(property.PropertyName);
+                if (lamda != null)
+                {
+                    ReflectionHelper.SetPropertyValue(this, property);
+                }
+                else
+                {
+                    await HandleExceptionAsync(new MixException(MixErrorStatus.Badrequest, $"Invalid Property {property.PropertyName}"));
+                }
+            }
+            IsValid = Validator.TryValidateObject(this, ValidateContext, Errors);
+            await Validate(cancellationToken);
+            var entity = await ParseEntity(cancellationToken);
+            await Repository.SaveAsync(entity, cancellationToken);
+            await CompleteUowAsync(cancellationToken);
+            return entity.Id;
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(ex);
+            return default;
+        }
+        finally
+        {
+            await CloseUowAsync();
+        }
+    }
+
+    #region virtual methods
+
+    // Override this method
+    protected virtual async Task<TEntity> SaveHandlerAsync(CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        var entity = await ParseEntity(cancellationToken);
+
+        ModifiedEntities.Add(new(typeof(TEntity), Id, !Id.Equals(default) ? ViewModelAction.Create : ViewModelAction.Update));
+
+        await Repository.SaveAsync(entity, cancellationToken);
+        await SaveEntityRelationshipAsync(entity, cancellationToken);
+        Id = entity.Id;
+        return entity;
+    }
+
+    // Override this method
+    protected virtual Task SaveEntityRelationshipAsync(TEntity parentEntity, CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        return Task.CompletedTask;
+    }
+
+    protected virtual Task DeleteEntityRelationshipAsync(CancellationToken cancellationToken = default)
+    {
+        return Task.CompletedTask;
+    }
+
+    #endregion
+
+    #endregion
+}
 }
