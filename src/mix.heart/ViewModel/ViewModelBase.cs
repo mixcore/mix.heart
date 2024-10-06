6dc861d462318a7e67ddda9d0cc5725b0f3516e0
phat <phat>
2024-10-06 23:33:43 +0700

    Remove redundant code

--- src/mix.heart/ViewModel/ViewModelBase.cs
+++ src/mix.heart/ViewModel/ViewModelBase.cs
@@ -1,13 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Mix.Heart.Entities;
 using Mix.Heart.Enums;
-using Mix.Heart.Exceptions;
-using Mix.Heart.Helpers;
 using Mix.Heart.UnitOfWork;
 using System;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Mix.Heart.ViewModel
 {
@@ -55,8 +50,9 @@ namespace Mix.Heart.ViewModel
 
         #endregion
 
-        #region Abstracts
-        public virtual void InitDefaultValues(string language = null, int? cultureId = null)
+        #region Overrides
+
+        public override void InitDefaultValues(string language = null, int? cultureId = null)
         {
             CreatedDateTime = DateTime.UtcNow;
             Status = MixContentStatus.Published;
@@ -64,70 +60,5 @@ namespace Mix.Heart.ViewModel
         }
 
         #endregion
-
-        public virtual async Task Validate(CancellationToken cancellationToken)
-        {
-            cancellationToken.ThrowIfCancellationRequested();
-            if (!IsValid)
-            {
-                await HandleExceptionAsync(new MixException(MixErrorStatus.Badrequest, Errors.Select(e => e.ErrorMessage).ToArray()));
-            }
-        }
-
-        public void SetDbContext(TDbContext context)
-        {
-            UowInfo = new UnitOfWorkInfo(context);
-        }
-
-        public virtual TEntity InitModel()
-        {
-            Type classType = typeof(TEntity);
-            return (TEntity)Activator.CreateInstance(classType);
-        }
-
-        public virtual Task<TEntity> ParseEntity(CancellationToken cancellationToken = default)
-        {
-            cancellationToken.ThrowIfCancellationRequested();
-
-            if (IsDefaultId(Id))
-            {
-                InitDefaultValues();
-            }
-
-            var entity = Activator.CreateInstance<TEntity>();
-            ReflectionHelper.Map(this as TView, entity);
-            return Task.FromResult(entity);
-        }
-
-        public bool IsDefaultId(TPrimaryKey id)
-        {
-            return (id.GetType() == typeof(Guid) && Guid.Parse(id.ToString()) == Guid.Empty)
-                || (id.GetType() == typeof(int) && int.Parse(id.ToString()) == default);
-        }
-
-        public virtual Task DuplicateAsync(CancellationToken cancellationToken = default)
-        {
-            cancellationToken.ThrowIfCancellationRequested();
-            return Task.CompletedTask;
-        }
-
-        public virtual void Duplicate()
-        {
-        }
-
-        protected async Task HandleErrorsAsync()
-        {
-            await HandleExceptionAsync(new MixException(MixErrorStatus.Badrequest, Errors.Select(e => e.ErrorMessage).ToArray()));
-        }
-
-        protected virtual async Task HandleExceptionAsync(Exception ex)
-        {
-            await Repository.HandleExceptionAsync(ex);
-        }
-
-        protected virtual void HandleException(Exception ex)
-        {
-            Repository.HandleException(ex);
-        }
     }
 }
