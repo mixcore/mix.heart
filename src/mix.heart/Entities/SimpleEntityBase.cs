c9b9f788cd7e6b7a5e32014161903beb7b30a67d
phat <phat>
2024-12-06 12:41:20 +0700

    Add simple entity base

---
+++ src/mix.heart/Entities/SimpleEntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mix.Heart.Entities
+{
+    public abstract class SimpleEntityBase<TPrimaryKey> : IEntity<TPrimaryKey> where TPrimaryKey : IComparable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public TPrimaryKey Id { get; set; }
+    }
+}
