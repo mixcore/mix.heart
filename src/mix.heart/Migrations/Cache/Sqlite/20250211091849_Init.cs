24eef36843372b1403f403c9b35d007c8b66c6e5
Nhật Hoàng <nhathoang.nguyen@pizza4ps.com>
2025-02-11 16:19:04 +0700

    update cache migration

---
+++ src/mix.heart/Migrations/Cache/Sqlite/20250211091849_Init.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mix.Heart.Migrations.Cache.Sqlite
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MixCache",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    keyword = table.Column<string>(type: "varchar(400)", nullable: false),
+                    value = table.Column<string>(type: "text", nullable: false),
+                    expired_date_time = table.Column<DateTime>(type: "datetime", nullable: true),
+                    modified_by = table.Column<string>(type: "varchar(50)", nullable: true),
+                    created_by = table.Column<string>(type: "varchar(50)", nullable: true),
+                    last_modified = table.Column<DateTime>(type: "datetime", nullable: true),
+                    created_date_time = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Priority = table.Column<int>(type: "INTEGER", nullable: false),
+                    status = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MixCache", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "index_expires_at_time",
+                table: "MixCache",
+                column: "expired_date_time");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MixCache");
+        }
+    }
+}
